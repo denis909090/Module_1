6606dae1bfb17000c4983d0dcc09295672fbfcde
Денис <deniiisgirman@gmail.com>
2026-03-11 14:07:36 +0200

    task higher difficulty

---
+++ pr2.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace pr2
+{
+    class prorgam_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Гирман Денис");
+        }
+    }
+}
