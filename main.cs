e248e0087a15b5e58e744160889f66b58bbd779f
denis909090 <135863448+denis909090@users.noreply.github.com>
2026-03-24 11:05:23 +0200

    this is common file for other files

--- main.cs
+++ main.cs
@@ -1,31 +1,14 @@
 ﻿using System;
 using Project.lab_1;
 
-// using Project.pract_1;
-// using Project.pract_2;
-
 namespace Project
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // base_tasks.task_1();
-            // base_tasks.task_2();
-            // base_tasks.task_3();
-            // base_tasks.task_4();
-            // base_tasks.task_5();
-
-            // individual_tasks.task_1();
-            // individual_tasks.task_2();
-            // individual_tasks.task_3();
-            // individual_tasks.task_4();
-            // individual_tasks.task_5();
-            // individual_tasks.task_6();
-            // individual_tasks.task_7();
-            trainee_task.elem();
-
-            // lab_tasks.task_2();
+            BaseTasks_2.task_3();
+            // BaseTasks_2.task_4();
         }
     }
 }
