3873492769c359ec4ab3ea589c926d7190d3e7a5
denis909090 <135863448+denis909090@users.noreply.github.com>
2026-05-10 21:58:12 +0300

    complete 3 and 4 tasks

--- main.cs
+++ main.cs
@@ -7,8 +7,18 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Гирман Денис");
-            Base_tasks_5.task_2();
+            // Console.WriteLine("Гирман Денис");
+            // Console.WriteLine("Введіть перше число:");
+            // int number1 = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Введіть друге число:");
+            // int number2 = int.Parse(Console.ReadLine());
+            // Base_tasks_5.task_2(number1, number2);
+            Console.WriteLine("Введіть пору року українською:");
+            string season = Console.ReadLine();
+
+            string result = Base_tasks_5.task_4(season);
+            Console.WriteLine(result);
         }
     }
 }
--- practical_work_5/base_tasks_5.cs
+++ practical_work_5/base_tasks_5.cs
@@ -4,14 +4,25 @@ namespace Project
 {
     class Base_tasks_5
     {
-        public static void task_2()
+        // public static void task_3()
+        // {
+        //     Console.WriteLine("Введіть перше число:");
+        //     int number1 = int.Parse(Console.ReadLine());
+
+        //     Console.WriteLine("Введіть друге число:");
+        //     int number2 = int.Parse(Console.ReadLine());
+
+        //     if (number1 % number2 == 0)
+        //     {
+        //         Console.WriteLine("Число " + number1 + " є кратним числу " + number2);
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Число " + number1 + " не є кратним числу " + number2);
+        //     }
+        // }
+        public static void task_2(int number1, int number2)
         {
-            Console.WriteLine("Введіть перше число:");
-            int number1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введіть друге число:");
-            int number2 = int.Parse(Console.ReadLine());
-
             if (number1 % number2 == 0)
             {
                 Console.WriteLine("Число " + number1 + " є кратним числу " + number2);
@@ -19,6 +30,30 @@ namespace Project
             else
             {
                 Console.WriteLine("Число " + number1 + " не є кратним числу " + number2);
+            }
+        }
+
+        public static string task_4(string season)
+        {
+            if (season == "весна")
+            {
+                return "Spring";
+            }
+            else if (season == "літо")
+            {
+                return "Summer";
+            }
+            else if (season == "осінь")
+            {
+                return "Autumn";
+            }
+            else if (season == "зима")
+            {
+                return "Winter";
+            }
+            else
+            {
+                return "Варіант перекладу відсутній";
             }
         }
     }
